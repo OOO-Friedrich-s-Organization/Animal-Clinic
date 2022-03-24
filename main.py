f4fb6e4e494ff7f45f3df773a657dc87bbbe2877
DenisAvocado <shelden2005@mail.ru>
2022-03-24 23:25:31 +0600

    To Heroku 4

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 def building():
     return "<h1>Здесь строится ветеринарная клиника!</h1>" \
            '<img src="static/build.jpg")'\
-           "alt='стройка идет' width='300' height='300'>"
+           "alt='стройка идет' width='300' height='300'>" \
+           "<h2>Мы будем по адресу ул.10 лет Октября, 41!</h2>"\
+           '<img src="static/map.jpg")'\
+           "alt='адрес' width='300' height='300'>"
 
 
 if __name__ == '__main__':
